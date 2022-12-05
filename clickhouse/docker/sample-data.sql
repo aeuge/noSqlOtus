e29ec9924807095e3822d88f65031b13ffd2a7e5
v.anashkin <v.anashkin@tinkoff.ru>
2022-12-05 20:23:25 +0300

    5 homework: clickhouse

---
+++ clickhouse/docker/sample-data.sql
@@ -0,0 +1,44 @@
+INSERT INTO test_stg.uk_price_paid
+WITH splitByChar(' ', postcode) AS p
+SELECT toUInt32(price_string) AS price,
+       parseDateTimeBestEffortUS(time) AS date,
+    p[1] AS postcode1,
+    p[2] AS postcode2,
+    transform(a, ['T', 'S', 'D', 'F', 'O'], ['terraced', 'semi-detached', 'detached', 'flat', 'other']) AS type,
+    b = 'Y' AS is_new,
+    transform(c, ['F', 'L', 'U'], ['freehold', 'leasehold', 'unknown']) AS duration,
+    addr1,
+    addr2,
+    street,
+    locality,
+    town,
+    district,
+    county
+FROM url(
+    'http://prod.publicdata.landregistry.gov.uk.s3-website-eu-west-1.amazonaws.com/pp-complete.csv',
+    'CSV',
+    'uuid_string String,
+    price_string String,
+    time String,
+    postcode String,
+    a String,
+    b String,
+    c String,
+    addr1 String,
+    addr2 String,
+    street String,
+    locality String,
+    town String,
+    district String,
+    county String,
+    d String,
+    e String'
+    ) SETTINGS max_http_get_redirects=10;
+
+SELECT toYear(date) AS year,
+   round(avg(price)) AS price,
+   bar(price, 0, 1000000, 80
+   )
+FROM uk_price_paid
+GROUP BY year
+ORDER BY year;
