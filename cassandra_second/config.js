1408632c7f03972024cb410189668576ea1d5b6f
v.anashkin <v.anashkin@tinkoff.ru>
2023-04-04 19:54:04 +0300

    homework: cassandra second

---
+++ cassandra_second/config.js
@@ -0,0 +1,14 @@
+{
+    "backup_name": "cassandra_backup",
+    "backup_dir": "/mnt/backups",
+    "data_dir": "/var/lib/cassandra/data",
+    "keyspace": "keyspace",
+    "s3_bucket": "s3_bucket",
+    "s3_region": "s3_region",
+    "s3_access_key_id": "s3_access_key_id",
+    "s3_secret_access_key": "s3_secret_access_key",
+    "cassandra_host": "node1",
+    "cassandra_port": "9042",
+    "cassandra_username": "",
+    "cassandra_password": ""
+}
